c332221a7e70b97064c3177129ed99e3984fece7
Daniel Nascimento <danrnascimento@gmail.com>
2021-04-04 02:02:24 -0300

    working

--- src/models/Color.ts
+++ src/models/Color.ts
@@ -1,16 +1,6 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToMany,
-  PrimaryColumn,
-} from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { IColor } from '../core/color/entity';
-import { IPallet } from '../core/pallet/entity';
-import { UserModel } from './User';
 
 @Entity('colors')
 export class ColorModel implements IColor {
